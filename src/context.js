a288e0d95a52ad8a7aff1609ac89ea0b0ebcc029
Roosevelt Innocent <howdy.roosevelt@gmail.com>
2019-09-19 21:30:47 +0100

    Details Page WIP

--- src/context.js
+++ src/context.js
@@ -5,7 +5,8 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
   state = {
     products: [],
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart: []
   };
 
   componentDidMount() {
@@ -23,14 +24,31 @@ class ProductProvider extends Component {
     });
   };
 
-  handleDetail = () => {
-    console.log("hello detail");
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
   };
 
-  addToCart = () => {
-    console.log("hello cart");
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
-  tester;
+
+  addToCart = (id) => {
+    let tempProducts  = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(()=> {
+      return { products: tempProducts, cart [...this.state.cart, ]}
+    })
+  };
+  
   render() {
     return (
       <ProductContext.Provider
